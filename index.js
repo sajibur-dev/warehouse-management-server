a21aa593548c9b80a3b0fb0b53ccde4a6d876944
sajibur-dev <sajibur.dev@gmail.com>
2022-04-29 11:38:09 +0600

    connected to mongodb database done

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 
+// middleware : 
+app.use(cors());
+app.use(express.json());
+
+
+
+// connected to database : 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.9gh47.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const productCollection = client.db("warehouse").collection("product");
+        console.log('db connected');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+run();
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('warehouse management system is running')
+})
 
 app.listen(port,()=>console.log('server is running'));
 
