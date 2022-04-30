ed098c4ebb4d1818a4c81655857562251fda116d
sajibur-dev <sajibur.dev@gmail.com>
2022-04-30 13:18:25 +0600

    specific data get done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -27,10 +27,16 @@ const run = async () => {
 
         app.get('/products',async(req,res)=>{
             const query = {};
-
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products)
+        })
+
+        app.get('/products/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
         })
         
     } catch (error) {
